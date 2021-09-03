Fixed more bugs. Changed delta_t to be the actual time difference, and not just current time. Added scaling.

Tracking/GlobalNearestNeighbour.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 from Tracking.utils.Georeference import GeoReference
@@ -5,7 +7,7 @@ from Tracking.utils.ObservationTracks import Track
 from Tracking.HungarianAlgorithm.Hungarian import Hungarian
 from scipy.optimize import linear_sum_assignment
 
-gate_threshold = 30000000000000.0
+gate_threshold = 300
 
 
 def observation_within_gate(predicted_position, observations):
@@ -27,7 +29,7 @@ def observation_within_gate(predicted_position, observations):
         if np.linalg.norm(observation - predicted_position) < gate_threshold:
             within_gate.append(distance)
         else:
-            within_gate.append(30000000000001.0)
+            within_gate.append(301.0)
 
     return within_gate
 
@@ -36,6 +38,7 @@ class GlobalNearestNeighbour:
     """
     Implementation of Global Nearest Neighbour algorithm.
     """
+
     def __init__(self):
         """
         Initialises GNN algorithm by creating empty list of tracks.
@@ -106,6 +109,8 @@ class GlobalNearestNeighbour:
             return
         elif len(self.tracks) != 0 and len(observations) == 0:
             for track in self.tracks:
+                if (track.get_prior_x() == [[0.], [0.], [0.], [0.]]).all():
+                    continue
                 track.predict_position(delta_t)
             return
 
@@ -118,5 +123,8 @@ class GlobalNearestNeighbour:
 
         row_ind, col_ind = linear_sum_assignment(cost_matrix)
         for row, col in zip(row_ind, col_ind):
-            if cost_matrix[row, col] == 30000000000001.0:
+            if cost_matrix[row, col] == 301.0:
                 self.__add_track(observations[row], yaw, pitch, roll, lat, lon, alt, delta_t)
+
+    def get_tracks(self):
+        return self.tracks